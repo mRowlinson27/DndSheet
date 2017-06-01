b0bb2547d9756ad9db7d410728e10d1918fa655e
mRowlinson27 <michaelrowlinson27@gmail.com>
2017-06-01 22:25:53 +1000

    Added dynamic table class

---
+++ DnDCharacterSheet/DataEntryForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnDCharacterSheet
+{
+    class DataEntryForm : TableLayoutPanel
+    {
+        private List<Label> _labels;
+        public DataEntryForm(int numLabels)
+        {
+            _labels = new List<Label>();
+            Height = 40;
+            Dock = DockStyle.Top;
+            BackColor = Color.Aqua;
+            CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset;
+            
+            GrowStyle = TableLayoutPanelGrowStyle.AddColumns;
+            ColumnCount = ColumnStyles.Count;
+            RowStyles.Add(new RowStyle(SizeType.Percent, 50));
+            RowCount = RowStyles.Count;
+
+            for (int i = 0; i < numLabels; i++)
+            {
+                ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                ColumnCount = ColumnStyles.Count;
+
+                _labels.Add(new Label() { Text = $"{i}", Width = 10, Height = 13 });
+                _labels.ElementAt(i).BackColor = Color.Red;
+                if (i == 1)
+                    _labels.ElementAt(i).BackColor = Color.Green;
+                if (i == 2)
+                    _labels.ElementAt(i).BackColor = Color.Blue;
+                if (i == 3)
+                    _labels.ElementAt(i).BackColor = Color.Violet;
+                Controls.Add(_labels.ElementAt(i));
+            }
+
+            GrowStyle = TableLayoutPanelGrowStyle.AddRows;
+            RowStyles.Add(new RowStyle(SizeType.Percent, 50));
+            RowCount = RowStyles.Count;
+
+            for (int i = 0; i < numLabels; i++)
+            {
+                _labels.Add(new Label() { Text = $"{i}", Width = 10, Height = 13 });
+                _labels.ElementAt(i + 3).BackColor = Color.Violet;
+                if (i == 1)
+                    _labels.ElementAt(i + 3).BackColor = Color.Blue;
+                if (i == 2)
+                    _labels.ElementAt(i + 3).BackColor = Color.Green;
+                if (i == 3)
+                    _labels.ElementAt(i + 3).BackColor = Color.Red;
+                Controls.Add(_labels.ElementAt(i + 3));
+            }
+        }
+    }
+}
--- DnDCharacterSheet/MainFormProcessor.cs
+++ DnDCharacterSheet/MainFormProcessor.cs
@@ -25,6 +25,7 @@ namespace DnDCharacterSheet
         public void SetUpStatPage()
         {
             _centraLayoutPanel = _centralLayoutPanelFactory.Create();
+            _centraLayoutPanel.BackColor = Color.Aquamarine;
             _mainLayout.Controls.Add(_centraLayoutPanel, 0, 1);
         }
 
@@ -32,28 +33,7 @@ namespace DnDCharacterSheet
         {
             //mainLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
             _centraLayoutPanel.RowCount = _centraLayoutPanel.RowStyles.Count;
-
-            var addTable = new TableLayoutPanel();
-            addTable.GrowStyle = TableLayoutPanelGrowStyle.AddRows;
-            addTable.Dock = DockStyle.Fill;
-            addTable.BackColor = Color.Brown;
-            
-            var label = new Label();
-            label.Text = "test";
-            var label2 = new Label();
-            label2.Text = "test2";
-            var label3 = new Label();
-            label3.Text = "test3";
-
-            addTable.Controls.Add(label);
-            addTable.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            addTable.RowCount = addTable.RowStyles.Count;
-            addTable.Controls.Add(label2);
-            addTable.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            addTable.RowCount = addTable.RowStyles.Count;
-            addTable.Controls.Add(label3);
-
-            _centraLayoutPanel.Controls.Add(addTable);
+            _centraLayoutPanel.Controls.Add(new DataEntryForm(4));
         }
     }
 }
