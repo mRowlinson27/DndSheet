8e2788e5a0e43bfe6c3007d9903948408bf94125
Michael Rowlinson <michael.rowlinson@Honeywell.com>
2017-06-14 15:56:17 +1000

    Changing style to take shared interfaces and copy across

--- DataManipulation/StyleApplier.cs
+++ DataManipulation/StyleApplier.cs
@@ -6,6 +6,53 @@ namespace DataManipulation
 {
     public class StyleApplier<T> : IStyleApplier<T>
     {
+
+        public T Apply(T input, T style)
+        {
+            var output = input;
+
+            var propertyInfos = style.GetType().GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var outputInfo = output.GetType().GetProperty(propertyInfo.Name);
+
+                if (outputInfo != null && !IsNullOrDefault(propertyInfo.GetValue(style)))
+                {
+                    outputInfo.SetValue(output, Convert.ChangeType(propertyInfo.GetValue(style), propertyInfo.PropertyType), null);
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+            return output;
+        }
+
+        private bool IsNullOrDefault<Tin>(Tin argument)
+        {
+            if (argument == null || object.Equals(argument, default(Tin)))
+            {
+                return true;
+            }
+
+            var methodType = typeof(Tin);
+            if (Nullable.GetUnderlyingType(methodType) != null)
+            {
+                return false;
+            }
+
+            var argumentType = argument.GetType();
+            if (argumentType.IsValueType && argumentType != methodType)
+            {
+                var obj = Activator.CreateInstance(argument.GetType());
+                return obj.Equals(argument);
+            }
+
+            return false;
+        }
+
         public T Apply(T input, Dictionary<string, object> dictionary)
         {
             var output = input;
--- UnitTests/DataManipulationTests/StyleApplierTests.cs
+++ UnitTests/DataManipulationTests/StyleApplierTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DataManipulation;
 using FluentAssertions;
 using NUnit.Framework;
@@ -8,12 +9,48 @@ namespace UnitTests.DataManipulationTests
     [TestFixture]
     class StyleApplierTests
     {
-        private StyleApplier<Generic> _styleApplier;
+        private StyleApplier<IGeneric> _styleApplier;
 
         [SetUp]
         public void Setup()
         {
-            _styleApplier = new StyleApplier<Generic>();
+            _styleApplier = new StyleApplier<IGeneric>();
+        }
+
+        [Test]
+        public void Apply_GenericObjectPrototyped()
+        {
+            var propertyValue = "test";
+            var original = new Generic1();
+            var style = new Generic1() { Property = propertyValue };
+
+            _styleApplier.Apply(original, style);
+
+            original.Property.Should().Be(style.Property);
+        }
+
+        [Test]
+        public void Apply_PropertyIsDefault_NoChange()
+        {
+            var propertyValue = "test";
+            IGeneric original = new Generic1() { Property = propertyValue };
+            IGeneric style = new Generic1();
+
+            _styleApplier.Apply(original, style);
+
+            original.Property.Should().Be(propertyValue);
+        }
+
+        [Test]
+        public void Apply_NoProperty_Exception()
+        {
+            IGeneric original = new Generic1();
+            IGeneric style = new Generic2() { OneHasProperty = "test" };
+            
+            _styleApplier.Apply(original, style);
+
+            Assert.That(() => _styleApplier.Apply(original, style),
+                Throws.TypeOf<ArgumentException>());
         }
 
         [Test]
@@ -27,7 +64,7 @@ namespace UnitTests.DataManipulationTests
                 { propertyName, stringData}
             };
 
-            var response = _styleApplier.Apply(new Generic(), data);
+            var response = _styleApplier.Apply(new Generic1(), data);
 
             response.Property.Should().Be(stringData);
         }
@@ -44,7 +81,7 @@ namespace UnitTests.DataManipulationTests
                 { propertyName, stringData}
             };
 
-            var response = _styleApplier.Apply(new Generic(), data);
+            var response = _styleApplier.Apply(new Generic1(), data);
 
             response.Should().Be(null);
         }
@@ -60,7 +97,7 @@ namespace UnitTests.DataManipulationTests
                 { propertyName, intData}
             };
 
-            var response = _styleApplier.Apply(new Generic(), data);
+            var response = _styleApplier.Apply(new Generic1(), data);
 
             response.Should().Be(null);
         }
@@ -76,7 +113,7 @@ namespace UnitTests.DataManipulationTests
                 { propertyName, stringData}
             };
 
-            var generic = new Generic();
+            var generic = new Generic1();
 
             _styleApplier.Apply(generic, data);
 
@@ -84,8 +121,21 @@ namespace UnitTests.DataManipulationTests
         }
     }
 
-    internal class Generic
+    internal interface IGeneric
+    {
+        string Property { get; set; }
+    }
+
+    internal class Generic1 : IGeneric
     {
         public string Property { get; set; }
+        public string TypeCheck { get; set; }
+    }
+
+    internal class Generic2 : IGeneric
+    {
+        public string Property { get; set; }
+        public int TypeCheck { get; set; }
+        public string OneHasProperty { get; set; }
     }
 }
