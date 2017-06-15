cead52f74206825c4127fc919877c853a9da6f73
Michael Rowlinson <michael.rowlinson@Honeywell.com>
2017-06-15 16:34:06 +1000

    progress (?) on event Applying

---
+++ DataManipulation/EventApplier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataManipulation
+{
+    public class EventApplier<T>
+    {
+        public T Apply(T input, T style)
+        {
+            var output = input;
+            var styleEvents = style.GetType().GetEvents();
+
+            foreach (var styleEvent in styleEvents)
+            {
+                Console.WriteLine(styleEvent.Name);
+                //var test = typeof(T).GetField("events", BindingFlags.NonPublic | BindingFlags.Instance);
+                //styleEvent.GetRaiseMethod().CreateDelegate()
+                var eventDelegate =
+                    (MulticastDelegate)
+                        style.GetType()
+                            .GetField(styleEvent.Name, BindingFlags.Instance | BindingFlags.NonPublic)
+                            .GetValue(style);
+                //https://stackoverflow.com/questions/198543/how-do-i-raise-an-event-via-reflection-in-net-c
+                eventDelegate.Method.Invoke(eventDelegate.GetInvocationList()[0], new []{style, (object) default(T)});
+
+                var ctrlEventsCollection = (EventHandlerList)typeof(T)
+                                        .GetProperty("Event", BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance)
+                                        .GetValue(style, null);
+
+                var outputEvent = output.GetType().GetEvent(styleEvent.Name);
+                if (outputEvent != null)
+                {
+                    var test = styleEvent.GetAddMethod(true);
+//                    test.CreateDelegate(EventHandler, this);
+                    outputEvent.AddEventHandler(output, new EventHandler((sender, args) => Console.WriteLine("meow")));
+//                    outputEvent.AddEventHandler(output, Delegate.CreateDelegate(typeof(EventHandler), test));
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ UnitTests/DataManipulationTests/EventApplierTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataManipulation;
+using NUnit.Framework;
+
+namespace UnitTests.DataManipulationTests
+{
+    [TestFixture]
+    public class EventApplierTests
+    {
+        private EventApplier<IGenericEvents> _eventApplier;
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventApplier = new EventApplier<IGenericEvents>();
+        }
+
+        [Test]
+        public void Event_SubScriberCopied()
+        {
+            var original = new GenericEvent1();
+            var eventStyle = new GenericEvent1();
+            eventStyle.Event += DoSomething;
+
+            _eventApplier.Apply(original, eventStyle);
+
+            eventStyle.OnEvent();
+            original.OnEvent();
+        }
+
+        private void DoSomething(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+    internal interface IGenericEvents
+    {
+        event EventHandler Event;
+        event EventHandler Click;
+    }
+
+    internal class GenericEvent1 : IGenericEvents
+    {
+        public event EventHandler Event;
+        public event EventHandler Click;
+
+        public void OnEvent()
+        {
+            Event.Invoke(null, null);
+        }
+    }
+
+    internal class GenericEvent2 : IGenericEvents
+    {
+        public event EventHandler Event;
+        public event EventHandler Click;
+    }
+}
